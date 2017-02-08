c4a56199faef1ad6fd91cf03318ea9002512ed1c
Giuseppe Castiglione <g.m.a.castiglione@gmail.com>
2017-02-08 00:09:37 -0500

    Better docstrings

--- image_utils.py
+++ image_utils.py
@@ -2,7 +2,20 @@ import numpy as np
 
 from scipy.ndimage import filters
 
-def harrisResponse(im, sigma=3):
+def harris_response(im, sigma=3):
+    """
+    Find corners using the determinant of the autocorrelation function
+
+    Args:
+        im (numpy.ndarray) : grayscale im (width, height)
+
+    Kwargs:
+        sigma (float) : width of gaussian filter used for derivative estimation
+
+    Returns:
+        (numpy.ndarray) : Harris response matrix (width, height)
+    """
+
     #compute derivatives
     im_x = np.zeros(im.shape)
     im_x = filters.gaussian_filter(im, (sigma,sigma), (0,1), im_x)
@@ -20,7 +33,22 @@ def harrisResponse(im, sigma=3):
     
     return Wdet / Wtr
 
-def nonMaximalSuppression(index, points, shape, min_dist=10):
+def non_maximal_suppression(index, points, shape, min_dist=10):
+    """
+    Filter points based on the strength of their activation and their relative distances
+
+    Args:
+        index (numpy.ndarray): (x,y) coordinates of a given flat index (n_samples, 2), int
+        points (numpy.ndarray): (x,y) coordinates of the candidate points (n_points, 2), int
+        shape (tuple): shape of the array in which the points reside
+
+    Kwargs:
+        min_dist (int) : minimum distance between points
+
+    Returns:
+        filtered_coords (numpy.ndarray) : (x,y) coordinates of the selected points (n_points, 2), int
+    """
+
     # store allowed point locations in array
     allowed = np.zeros(shape)
     allowed[min_dist:-min_dist,min_dist:-min_dist] = 1
@@ -36,7 +64,20 @@ def nonMaximalSuppression(index, points, shape, min_dist=10):
             
     return np.asarray(filtered_coords)
 
-def response2points(harris, min_dist=10, threshold=0.1):
+def response_to_points(harris, min_dist=10, threshold=0.1):
+    """
+    Select harris response points via non maximal suppression
+
+    Args:
+        harris (numpy.ndarray): the harris response matrix (width, height)
+
+    Kwags:
+        min_dist (int) : minimum distance between points
+        threshold (float) : fraction of max, below which corner candidates are rejected (0-1)
+
+    Returns:
+        (numpy.ndarray) : Corner points, (n_points, 2)
+    """
     corner_thresh = np.max(harris) * threshold
     
     # Obtain candidate points and their harris values
@@ -44,7 +85,7 @@ def response2points(harris, min_dist=10, threshold=0.1):
     values = np.asarray([harris[p[0],p[1]] for p in points])
     index = np.argsort(values)
     
-    return nonMaximalSuppression(index, points, shape, min_dist)
+    return non_maximal_suppression(index, points, shape, min_dist)
 
 
 
--- stained_glass.py
+++ stained_glass.py
@@ -12,7 +12,7 @@ from scipy import ndimage, misc
 
 sns.set(color_codes=True)
 
-from image_utils import harrisResponse, response2points, nonMaximalSuppression
+from image_utils import harris_response, response_to_points, non_maximal_suppression
 
 from scipy.spatial import Delaunay
 
@@ -24,11 +24,26 @@ from skimage.morphology import disk
 from tqdm import tqdm
 
 def unique_rows(a):
+    """
+    Select the unique rows of a numpy array
+    """
     a = np.ascontiguousarray(a)
     unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
     return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
 
-def genRandomBorderPoints(max_n, min_dist, rng):
+def gen_random_border_points(max_n, min_dist, rng):
+    """
+    Select random points on the border that are more or less evenly spaced, 
+    including the corners
+
+    Args:
+        max_n (int): The end of the interval
+        min_dist (int): minimal separation between points
+        rng (numpy.random.RandomState) : numpy random number generator
+
+    Returns:
+        border (numpy.ndarray) : Array of border points
+    """
     candids = np.arange(min_dist, max_n - min_dist)
     border = [0, max_n]
     
@@ -45,7 +60,15 @@ def genRandomBorderPoints(max_n, min_dist, rng):
                                         candids > ind + min_dist)]
 
 
-def fillTriangle(tr, art_img, new_img):
+def fill_triangle(tr, art_img, new_img):
+    """
+    Fill a specified triangular region with the average color in that region
+
+    Args:
+        tr (numpy.ndarray): Triangle coordinates, (3,2)
+        art_img (numpy.ndarray): The original RGB image, (width, height, 3)
+        new_img (numpy.ndarray): Destination to save colored triangle
+    """
     r = cv2.boundingRect(np.float32([tr]))
     t_rec = []
     for i in xrange(0, 3):
@@ -63,11 +86,21 @@ def fillTriangle(tr, art_img, new_img):
     new_img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]][kx,ky,:] = vals[None,None,:] #* (mask[:,:,None])
     #art_img_patch * (mask[:,:,None] ) + 
 
-def genAllFilters(art):
+def gen_all_filters(art):
+    """
+    Generate all the filtered versions of the input image, for use in other functions
+
+    Args:
+        art (numpy.ndarray): The original RGB image, (width, height, 3)
+
+    Returns:
+        im_smooth (numpy.ndarray): Smoothed grayscale image (width, height)
+        smooth_grad_mag (numpy.ndarray): Gradient magnitude of smoothed, grayscale image (width, height)
+        loc_autolevel (numpy.ndarray): Rank-filtered grayscale image (width, height)
+        sharp_grad_mag (numpy.ndarray): Gradient magnitude of rank-filtered, grayscale image (width, height)
+    """
     #Convert to grayscale for edge estimation
     hsv_art = mpl.colors.rgb_to_hsv(art).astype(np.float32)
-
-
 
     #The smooth image
     im_smooth = np.zeros(hsv_art[:,:,0].shape)
@@ -98,7 +131,20 @@ def genAllFilters(art):
     return im_smooth, smooth_grad_mag, loc_autolevel, sharp_grad_mag
 
         
-def edgePointsWithBoundingBox(smooth_im, sharp_im, inds, min_dist):
+def edge_points_width_bounding_box(smooth_im, sharp_im, inds, min_dist):
+    """
+    Obtain critical points on the image and boundary
+
+    Args:
+        smooth_im (numpy.ndarray): Smoothed, grayscale version of the image (width, height)
+        sharp_im (numpy.ndarray): Higher-contrast, grayscale version of the image (width, height)
+        inds (numpy.ndarray): (x,y) coordinates of a given flat index (n_samples, 2), int
+        min_dist (int) : minimum distance between points
+
+    Returns:
+        points (numpy.ndarray): The (x,y) coordinates of the critical points, (n_points, 2)
+        p_edges (numpy.ndarray): Fuzzy boundary map around critical points in image (width, height)
+    """
     edges = feature.canny(smooth_im, sigma=1)
     edges2 = feature.canny(sharp_im, sigma=5)
     p_edges = edges2 - edges
@@ -109,12 +155,12 @@ def edgePointsWithBoundingBox(smooth_im, sharp_im, inds, min_dist):
     index = np.argsort(values)
 
     # # select the best points taking min_distance into account
-    filtered_coords = nonMaximalSuppression(index, edge_points, 
+    filtered_coords = non_maximal_suppression(index, edge_points, 
         smooth_im.shape, min_dist).tolist()
 
     rng = np.random.RandomState(223245)
-    x_range = genRandomBorderPoints(smooth_im.shape[0] - 1, min_dist, rng)
-    y_range = genRandomBorderPoints(smooth_im.shape[1] - 1, min_dist, rng)
+    x_range = gen_random_border_points(smooth_im.shape[0] - 1, min_dist, rng)
+    y_range = gen_random_border_points(smooth_im.shape[1] - 1, min_dist, rng)
 
     lft_row = [[0, yi] for yi in y_range]
     filtered_coords.extend(lft_row)
@@ -135,19 +181,42 @@ def edgePointsWithBoundingBox(smooth_im, sharp_im, inds, min_dist):
     return points, p_edges
 
 
-def stainedGlassTransform(art, points):
-    
+def stained_glass_transform(art, points):
+    """
+    Use Delaunay tesselation to create a crystalline average of an image
+
+    Args:
+        art (numpy.ndarray): The original RGB image, (width, height, 3)
+        points (numpy.ndarray): The (x,y) coordinates of the critical points, (n_points, 2)
+
+    Returns
+        new_image (numpy.ndarray): The transformed RGB image (width, height, 3)
+    """
     tri = Delaunay(points)
 
     new_image = np.zeros(art.shape)
 
     for i, region in tqdm(enumerate(tri.simplices), desc='regions'):
         vertices = points[region] 
-        fillTriangle(vertices, 256 - art, new_image)
+        fill_triangle(vertices, 256 - art, new_image)
 
     return new_image
 
-def addContours(art, inds, sharp_grad, smooth_grad):
+def add_contours(art, inds, sharp_grad, smooth_grad):
+    """
+    Increase stained-glass effect by adding a frame to the image
+
+    Args:
+        art (numpy.ndarray): The original RGB image, (width, height, 3)
+        inds (numpy.ndarray): (x,y) coordinates of a given flat index (n_samples, 2), int
+        smooth_grad (numpy.ndarray): Gradient magnitude of smoothed grayscale image (width, height)
+        sharp_grad (numpy.ndarray): Gradient magnitude of rank-filtered grayscale image (width, height)
+
+    Returns:
+        smooth_edge_points (numpy.ndarray): Location of the thin frame (n_pts, 2)
+        cast_edge_points (numpy.ndarray): Location of the thick frame (n_pts, 2)
+    """
+
     #Smooth edge contours
     smooth_grad_thresh = smooth_grad.copy()
     smooth_grad_thresh[smooth_grad_thresh < 1.2] = 0
@@ -163,7 +232,18 @@ def addContours(art, inds, sharp_grad, smooth_grad):
 
     return smooth_edge_points, cast_edge_points
 
-def genImage(art, stained_glass, cast_edge_points, smooth_edge_points, fname):
+def gen_image(art, stained_glass, cast_edge_points, smooth_edge_points, fname):
+    """
+    Generate and save the stained glass transform
+
+    Args:
+        art (numpy.ndarray): The original RGB image, (width, height, 3)
+        stained_glass (numpy.ndarray): The Delaunay transformed RGB image (width, height, 3)
+        cast_edge_points (numpy.ndarray): Location of the thick frame (n_pts, 2)
+        smooth_edge_points (numpy.ndarray): Location of the thin frame (n_pts, 2)
+        fname (str) : Directory to save image
+
+    """
     fig = plt.figure(figsize=(30,30))
     axs = fig.add_subplot(111)
 
@@ -181,6 +261,31 @@ def genImage(art, stained_glass, cast_edge_points, smooth_edge_points, fname):
     plt.savefig(fname, bbox_inches='tight')
     plt.show()
 
+def main(args):
+    art = misc.imread(args.art)
+
+    x = gen_all_filters(art)
+    smooth_im, smooth_grad, sharp_im, sharp_grad = x
+
+    x_range = np.arange(art.shape[0])
+    y_range = np.arange(art.shape[1])
+    xi, yi = np.meshgrid(x_range,y_range)
+    inds = np.array([xi.ravel(), yi.ravel()]).T
+
+    points, p_edges = edge_points_width_bounding_box(smooth_im, sharp_im, inds, args.min_dist)
+    stained_glass = stained_glass_transform(art, points)
+    stained_glass[p_edges] = 0
+
+    x_range = np.arange(art.shape[1])
+    y_range = np.arange(art.shape[0])
+    xi, yi = np.meshgrid(x_range,y_range)
+    inds = np.array([xi.ravel(), yi.ravel()]).T
+    smooth_edge_points, cast_edge_points = add_contours(art, inds, sharp_grad, smooth_grad)
+
+
+    gen_image(art, stained_glass, cast_edge_points, smooth_edge_points, args.output)
+
+
 import argparse
 
 if __name__ == "__main__":
@@ -192,27 +297,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    art = misc.imread(args.art)
-
-    x = genAllFilters(art)
-    smooth_im, smooth_grad, sharp_im, sharp_grad = x
-
-    x_range = np.arange(art.shape[0])
-    y_range = np.arange(art.shape[1])
-    xi, yi = np.meshgrid(x_range,y_range)
-    inds = np.array([xi.ravel(), yi.ravel()]).T
-
-    points, p_edges = edgePointsWithBoundingBox(smooth_im, sharp_im, inds, args.min_dist)
-    stained_glass = stainedGlassTransform(art, points)
-    stained_glass[p_edges] = 0
-
-    x_range = np.arange(art.shape[1])
-    y_range = np.arange(art.shape[0])
-    xi, yi = np.meshgrid(x_range,y_range)
-    inds = np.array([xi.ravel(), yi.ravel()]).T
-    smooth_edge_points, cast_edge_points = addContours(art, inds, sharp_grad, smooth_grad)
-
-
-    genImage(art, stained_glass, cast_edge_points, smooth_edge_points, args.output)
+    main(args)
 
 
